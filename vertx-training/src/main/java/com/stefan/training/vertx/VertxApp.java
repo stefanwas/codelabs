f6d8c0f74f1cbae1e4128355082a57c20dcd631b
stefanwas <swtemp001@gmial.com>
2017-01-03 17:29:52 +0100

    added Vertx dep

---
+++ vertx-training/src/main/java/com/stefan/training/vertx/VertxApp.java
@@ -0,0 +1,9 @@
+package com.stefan.training.vertx;
+
+import io.vertx.core.Vertx;
+
+public class VertxApp {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+    }
+}
