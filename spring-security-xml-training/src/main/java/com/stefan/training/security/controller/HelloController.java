66a9e6e78901f7f964580cc8f90b5a1c6366689c
stefanwas <swtemp001@gmail.com>
2017-03-22 23:35:38 +0100

    playing with spring sec 2

--- spring-security-xml-training/src/main/java/com/stefan/training/security/controller/HelloController.java
+++ spring-security-xml-training/src/main/java/com/stefan/training/security/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.stefan.training.security.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,8 +13,13 @@ import org.springframework.web.servlet.ModelAndView;
 //@RequestMapping("/")
 public class HelloController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+
     @RequestMapping(value = "/welcome", method = RequestMethod.GET)
     public ModelAndView welcomePage() {
+
+        LOGGER.info("Handling request: /welcome");
 
         ModelAndView model = new ModelAndView();
         model.addObject("title", "Spring Security Hello World");
@@ -23,10 +31,33 @@ public class HelloController {
 
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public ModelAndView adminPage() {
+
+        LOGGER.info("Handling request: /admin");
+
         ModelAndView model = new ModelAndView();
         model.addObject("title", "Spring Security Hello World");
         model.addObject("message", "This is admin page!");
         model.setViewName("admin");
+        return model;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView loginPage(@RequestParam(value = "error", required = false) String error,
+                                  @RequestParam(value = "logout", required = false) String logout) {
+
+        LOGGER.info("Handling request: /login");
+
+        if (error != null) {
+            LOGGER.info("!!! Login error: {}", error);
+        }
+        if (logout != null) {
+            LOGGER.info("!!! Logout: {}", error);
+
+        }
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is login page!");
+        model.setViewName("login");
         return model;
     }
 }
