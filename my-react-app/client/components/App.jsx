400eab737bc82a73dfa9c67b83c91f689c8d4703
stefanwas <swtemp001@gmail.com>
2017-08-20 20:00:40 +0200

    work on my-react-app

---
+++ my-react-app/client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+/*
+    ./client/components/App.jsx
+*/
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
--- my-react-app/client/index.js
+++ my-react-app/client/index.js
@@ -1,5 +1,8 @@
-/*
-    ./client/index.js
-    which is the webpack entry file
-*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 console.log('Hey guys!!!');
