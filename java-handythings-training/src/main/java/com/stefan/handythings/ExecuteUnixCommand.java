50247951fe8cddeaa4671a244b5d6473f9b831f2
stefanwas <swtemp001@gmail.com>
2017-10-18 14:36:58 +0200

    added HandyThings

---
+++ java-handythings-training/src/main/java/com/stefan/handythings/ExecuteUnixCommand.java
@@ -0,0 +1,26 @@
+package com.stefan.handythings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExecuteUnixCommand {
+
+    public static void main(String[] args) {
+        String cmd = "pwd";
+
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            process.waitFor();
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = stdInput.readLine()) != null) {
+                System.out.println("Line=" + line);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
