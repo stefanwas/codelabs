e7185cf1ef8e67c28e3777d223900eb3f799a4e0
stefanwas <swtemp001@gmail.com>
2017-10-18 14:38:43 +0200

    removed yarn.lock from repo

--- java-handythings-training/src/main/java/com/stefan/handythings/ExecuteUnixCommand.java
+++ java-handythings-training/src/main/java/com/stefan/handythings/ExecuteUnixCommand.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Executes a command line program and prints out its output
+ */
 public class ExecuteUnixCommand {
 
     public static void main(String[] args) {
